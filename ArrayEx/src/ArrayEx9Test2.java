5be7c3cf532b7e9c3af25222866745d120ff4977
Hyemi Jung <hyemijung84@gmail.com>
2018-12-31 07:45:11 +0900

    Array
    math random

--- ArrayEx/src/ArrayEx9Test2.java
+++ ArrayEx/src/ArrayEx9Test2.java
@@ -8,12 +8,34 @@ public class ArrayEx9Test2 {
 
 		Scanner scan = new Scanner(System.in);
 		
-		int[] num = new int[5];
+		int[] rndNum = new int[5];
+		
+		rndNum = scan.next();
+		
+		System.out.println("입력시 5개의 숫자는 겹쳐서 넣으면 안됩니");
+		System.out.println();
+		System.out.println("1번째 숫자를 입력하세요");
+		System.out.println();
+		System.out.println("2번째 숫자를 입력하세요");
+		System.out.println();
+		System.out.println("3번째 숫자를 입력하세요");
+		System.out.println();
+		System.out.println("4번째 숫자를 입력하세요");
+		System.out.println();
+		System.out.println("5번째 숫자를 입력하세요");
+		System.out.println();
+		
+	 int temp = 0;
+	 int rndNum2 = 0;
+	 
 		
 		for (int i = 0; i < 5; i++) {
-			num = (int)(Math.random() * 5);
-		
-			num = scan.nextInt();
+			rndNum = (int)(Math.random() * 5);
+			temp = rndNum[i];
+			rndNum[i] = rndNum[rndNum2];
+			rndNum[rndNum2] = temp;
+		}
+			
 		
 		
 		
