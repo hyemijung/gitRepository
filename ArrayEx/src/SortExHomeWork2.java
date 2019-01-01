abec5b74ea58886365fa39e1538c24f0d1b6a6e4
Hyemi Jung <hyemijung84@gmail.com>
2019-01-02 07:39:18 +0900

    Array
    Array 다차원 배열

---
+++ ArrayEx/src/SortExHomeWork2.java
@@ -0,0 +1,22 @@
+
+public class SortExHomeWork2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		2차원 배열 생성과 선언 
+		int[][] gugudan = new int[8][8];	
+
+//	2차원 배열 할당 
+		for (int i = 2; i < gugudan.length; i++) {
+			for (int n = 1; n < gugudan.length; n++) {
+				gugudan[i][n] = i * n;
+			}
+			System.out.print(i + "*" + n + "=" + gugudan[i][n]);
+			
+		}
+		
+		
+	}
+
+}
